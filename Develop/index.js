26340fa477f1f4ef70d1204faf377edeaba5a7a2
Sibo_Fulgencio <eduardo.pacheco@utexas.edu>
2022-08-20 17:25:58 -0500

    Update of Inquirer Questionnaire.
    Questions are prompted according to the selection of the user.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,108 @@
 // TODO: Include packages needed for this application
 
+const path = 'C:/xampp/htdocs/Bootcamp/TDM-VIRT-FSF-PT-06-2022-U-LOLC/09-NodeJS/01-Activities/18-Stu_Package-npm/Unsolved/node_modules/inquirer'
+
+const inquirer = require(path);
+const fs = require('fs');
+
+const textGen =  require('./utils/generateMarkdown.js');
+
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the Title of your Project',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username',
+      },
+      {
+        type: 'input',
+        name: 'mail',
+        message: 'Please enter your e-mail',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'PLease choose the License that applies to your project',
+        choices: ['None','Apache 2.0', 'General Public License v3.0', 'MIT', 'ISC License'],
+      },
+      {
+        type: 'checkbox',
+        name: 'sections',
+        message: 'Please indicate the sections to include in your README file?',
+        choices: ['Installation', 'Usage', 'Contributing Process', 'Tests']
+      },
+      {
+        type: 'editor',
+        name: 'description',
+        message: 'Please enter the description of your project'
+  
+      },
+      {
+        type: 'editor',
+        name: 'Installation',
+        message: 'Please enter Installation instructions',
+        when: (answers) => answers.sections.filter(param => {return param == 'Installation'}).length == 1
+      },
+      {
+        type: 'editor',
+        name: 'Usage',
+        message: 'Please enter the usage Instructions for the Application',
+        when: (answers) => answers.sections.filter(param => {return param == 'Usage'}).length == 1
+      },
+      {
+        type: 'editor',
+        name: 'Contributing Process',
+        message: 'Please describe how to make contributions',
+        when: (answers) => answers.sections.filter(param => {return param == 'Contributing Process'}).length == 1
+      },
+      {
+        type: 'editor',
+        name: 'Tests',
+        message: 'Please describe the Tests used for this Application ',
+        when: (answers) => answers.sections.filter(param => {return param == 'Tests'}).length == 1
+      }
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    console.log(data);
+    
+
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Successfully created README.MD!')
+  );
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions)
+    .then((answers) => {       
+
+      // console.log('readMetext', answers)
+      const readMetext = textGen.generateMarkdown(answers);
+        
+      writeToFile('README_PROF.md',readMetext)
+
+    })
+
+}
 
 // Function call to initialize app
+
+
+
 init();
+
+
